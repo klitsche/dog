8739f156c258c70973c75f8ef7ba3cfca57577a9
Dirk Adler <dirx@klitsche.de>
2020-08-21 21:37:17 +0200

    Fix ecs config
    * plus: use php instead of yaml

---
+++ ecs.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(
+        'sets',
+        [
+            'psr-12',
+            'php-70',
+            'php-71',
+            'common',
+            'clean-code',
+            'dead-code',
+        ]
+    );
+    $parameters->set(
+        'skip',
+        [
+            \PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer::class => '~',
+            \PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer::class => '~',
+        ]
+    );
+    $parameters->set(
+        'exclude_files',
+        [
+            'tests/Dummy/*'
+        ]
+    );
+
+    $services = $containerConfigurator->services();
+    $services->set(OrderedImportsFixer::class)
+        ->call(
+            'configure',
+            [
+                [
+                    'imports_order' => [
+                        'class',
+                        'const',
+                        'function',
+                    ],
+                ]
+            ]
+        );
+};
