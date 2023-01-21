bf248742b1da97cf2704864ab6718f04f1e07529
Dirk Adler <dirx@klitsche.de>
2023-01-21 23:02:03 +0100

    Drop PHP 7.4 support & dump deps

--- box.php-scoper.php
+++ box.php-scoper.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 
 return [
-    'whitelist' => [
-        'Klitsche\Dog\*',
-        'Twig\*',
+    'exclude-namespaces' => [
+        'Klitsche\Dog',
+        'Twig',
     ],
 ];
--- ecs.php
+++ ecs.php
@@ -5,37 +5,27 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
 use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
 use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::PSR_12);
-    $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import(SetList::CLEAN_CODE);
+return static function (ECSConfig $config): void {
+    $config->sets([
+        SetList::PSR_12,
+        SetList::COMMON,
+        SetList::CLEAN_CODE,
+    ]);
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::SKIP,
-        [
-            'tests/Dummy/*',
-            ClassAttributesSeparationFixer::class => '~',
-            OrderedClassElementsFixer::class => '~',
-        ]
-    );
+    $config->skip([
+        'tests/Dummy/*',
+        ClassAttributesSeparationFixer::class => '~',
+        OrderedClassElementsFixer::class => '~',
+    ]);
 
-    $services = $containerConfigurator->services();
-    $services->set(OrderedImportsFixer::class)
-        ->call(
-            'configure',
-            [
-                [
-                    'imports_order' => [
-                        'class',
-                        'const',
-                        'function',
-                    ],
-                ]
-            ]
-        );
+    $config->ruleWithConfiguration(OrderedImportsFixer::class, [
+        'imports_order' => [
+            'class',
+            'const',
+            'function',
+        ],
+    ]);
 };
